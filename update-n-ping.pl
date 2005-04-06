3341551c599e24ca859d0b78b6f49e4e3003efc8
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2005-04-06 10:36:09 +0000

    Bug fix, and a fix to avoid meaningless update pings
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-Update_n_Ping/trunk@3 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- update-n-ping.pl
+++ update-n-ping.pl
@@ -1,6 +1,6 @@
 # A plugin for sending "update pings" when updating entries
 #
-# Release 0.01 (Feb 23, 2005)
+# Release 0.02 (Feb 24, 2005)
 #
 # This software is provided as-is. You may use it for commercial or 
 # personal use. If you distribute it, please keep this notice intact.
@@ -10,12 +10,13 @@
 use strict;
 our $urls = [
 	     'http://rpc.pingomatic.com/'
-	    ];
+	     ];
+our $LIMIT_ENTRIES = 15;
 
 if (MT->can('add_plugin')) {
   require MT::Plugin;
   my $plugin = new MT::Plugin();
-  $plugin->name("Update-n-Ping Plugin 0.01");
+  $plugin->name("Update-n-Ping Plugin 0.02");
   $plugin->description("Send 'update pings' when updating entries");
   $plugin->doc_link("http://as-is.net/hacks/2005/02/update_n_ping_plugin.html");
   MT->add_plugin($plugin);
@@ -27,8 +28,23 @@ if (MT->can('add_callback')) {
 
 sub update_n_ping {
   my ($eh, $app, $new_entry) = @_;
-  return if (!$new_entry->allow_pings ||
-	     $new_entry->status != MT::Entry::RELEASE());
+  return if $new_entry->status != MT::Entry::RELEASE();
+
+  if ($LIMIT_ENTRIES) {
+    my $iter = MT::Entry->load_iter({ blog_id => $new_entry->blog_id,
+				      status => MT::Entry::RELEASE() },
+				    { sort => 'created_on',
+				      direction => 'descend',
+				      limit => $LIMIT_ENTRIES });
+    my $is_recent = 0;
+    while (my $e = $iter->()) {
+      if ($e->id == $new_entry->id) {
+	$is_recent = 1;
+	last;
+      }
+    }
+    return unless $is_recent;
+  }
 
   require MT::Blog;
   require MT::XMLRPC;
@@ -39,7 +55,7 @@ sub update_n_ping {
     if (MT::XMLRPC->ping_update('weblogUpdates.ping', $blog, $url)) {
       $msg .= 'suceeded.';
     } else {
-      $msg .= 'failed.';
+      $msg .= 'failed. ' . MT::XMLRPC->errstr;
     }
     $app->log($msg);
   }
